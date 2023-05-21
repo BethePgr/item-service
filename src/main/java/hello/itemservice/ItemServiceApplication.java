226f4398f7cffd63355d4b359cf6ee3efedbde5d
jongwonpc <c3392302@naver.com>
2023-05-21 21:05:30 +0900

    상품 도메인 개발

---
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -0,0 +1,16 @@
+package hello.itemservice;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemServiceApplication.class, args);
+	}
+}
+
