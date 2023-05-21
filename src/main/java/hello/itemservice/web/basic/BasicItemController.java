24b8d22ef3aaf0e87e4b903090e38e28360ef30d
jongwonpc <c3392302@naver.com>
2023-05-21 23:17:08 +0900

    PRG Post/Redirect/Get

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -69,11 +69,18 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String addItemV4(Item item) {
         itemRepository.save(item);
         //model.addAttribute("item",item); //자동추가되므로 생략 가능,
         return "basic/item";
+    }
+
+    @PostMapping("/add")
+    public String addItemV5(Item item) {
+        itemRepository.save(item);
+        //model.addAttribute("item",item); //자동추가되므로 생략 가능,
+        return "redirect:/basic/items/" + item.getId();
     }
 
     @GetMapping("/{itemId}/edit")
